2dc9cb3af83d855bcac73ff6e7312d6ca6c3d60b
alex520biao <alex520biao@163.com>
2013-06-07 18:19:48 +0800

    Initial Commit

---
+++ ErrorManager/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ErrorManager
+//
+//  Created by alex on 13-6-7.
+//  Copyright (c) 2013年 alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
